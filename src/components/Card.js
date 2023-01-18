840ab4f4faf69a0eb7ffbfafee443a5a6377c96c
jholloway <jholloway@griddynamics.com>
2023-01-18 11:34:03 -0500

    add horizonatal rule to Card.js and styling to App.css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,20 @@
 export default function Card(props){
     return(
-        <div className="card-container">
-            <div className="img-container">
-                <img className="location-img" src={props.imageUrl} alt={props.title}/>
-            </div>
-            <div className="details">
-                <div className="location">
-                    <span className="fa fa-map-marker"></span>
-                    <span className="country">{props.location}</span>
-                    <span><a className="map-link" href={props.googleMapsUrl}>View on Google Maps</a></span>
+        <div>
+            <div className="card-container">
+                <div className="img-container">
+                    <img className="location-img" src={props.imageUrl} alt={props.title}/>
                 </div>
-                <h2 className="place-name">{props.title}</h2>
-                <h4 className="dates">{props.startDate} - {props.endDate}</h4>
-                <p className="description">{props.description}</p>
+                <div className="details">
+                    <div className="location">
+                        <span className="fa fa-map-marker"></span>
+                        <span className="country">{props.location}</span>
+                        <span><a className="map-link" href={props.googleMapsUrl}>View on Google Maps</a></span>
+                    </div>
+                    <h2 className="place-name">{props.title}</h2>
+                    <h4 className="dates">{props.startDate} - {props.endDate}</h4>
+                    <p className="description">{props.description}</p>
+                </div>
             </div>
             <hr></hr>
         </div>
