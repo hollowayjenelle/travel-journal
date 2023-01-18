04ad783a695651862feecc5102f6371e2b7a1f37
jholloway <jholloway@griddynamics.com>
2023-01-18 11:01:12 -0500

    add styling to Card component; add Card components to App.js using props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Card from './components/Card';
+import data from './data'
 
 function App() {
+  const cards = data.map(obj => {
+    return(
+      <Card 
+        key={obj.title}
+        {...obj}
+      />
+    )
+  })
+
   return (
     <div className="App">
       <Navbar />
+      <section className='cards'>
+        {cards}
+      </section>
     </div>
   );
 }
